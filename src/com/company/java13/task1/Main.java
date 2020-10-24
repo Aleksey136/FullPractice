6073b7e04af48dc64659fb2260409333e9a89e5a
Aleksey136 <2002Lesha>
2020-10-24 15:59:50 +0300

    added

---
+++ src/com/company/java13/task1/Main.java
@@ -0,0 +1,24 @@
+package com.company.java13.task1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        /*String input = scanner.nextLine();*/
+        String input = "Вася заработал 50000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        char[] inputChar = new char[100];
+        input.getChars(input.lastIndexOf("Вася") + 15, input.indexOf(" рублей, Петя") , inputChar , 0);
+        String strVasya = "";
+        for (Character c : inputChar)
+            strVasya += c.toString();
+        int intVasya = Integer.parseInt(strVasya.trim());
+        char[] inputChar2 = new char[100];
+        input.getChars(input.lastIndexOf("Маша") + 7, input.lastIndexOf(" рублей") , inputChar2 , 0);
+        String strMasha = "";
+        for (Character c : inputChar2)
+            strMasha += c.toString();
+        int intMasha = Integer.parseInt(strMasha.trim());
+        System.out.println("\n" + (intVasya+intMasha));
+    }
+}
---
+++ src/com/company/java13/task2/Main.java
@@ -0,0 +1,63 @@
+package com.company.java13.task2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        /*String input = scanner.nextLine();*/
+        String input = "Иванов Сергей Петрович";
+        String firstName = "";
+        String lastName = "";
+        String patronymicName = "";
+        char[] inputChar = new char[100];
+        for (int i = 0; i < input.length(); i++)
+            inputChar[i] = input.charAt(i);
+        int quantityChar = 0;
+        for (int i = 0; i < input.length(); i++)
+            if (inputChar[i] == ' ')
+                quantityChar++;
+        if (quantityChar == 0 || quantityChar > 2) {
+            System.out.println("Введенная строка не является ФИО");
+        }
+        else if (quantityChar == 1) {
+            int placeSpace = 0;
+            for (int i = 0; i < input.length(); i++) {
+                if (inputChar[i] == ' ')
+                    placeSpace = i;
+            }
+            for (int i = 0; i < placeSpace; i++) {
+                lastName += inputChar[i];
+            }
+            for (int i = placeSpace+1; i < input.length(); i++) {
+                firstName += inputChar[i];
+            }
+            System.out.println("Фамилия: " + lastName + "\nИмя: " + firstName);
+        }
+        else if (quantityChar == 2) {
+            int placeSpace1 = -1;
+            int placeSpace2 = -2;
+            for (int i = 0; i < input.length(); i++) {
+                if (inputChar[i] == ' ')
+                    placeSpace2 = i;
+            }
+            for (int i = 0; i < input.length(); i++) {
+                if (inputChar[i] == ' '){
+                    placeSpace1 = i;
+                    if (placeSpace1!=placeSpace2)
+                        break;
+                }
+            }
+            for (int i = 0; i < placeSpace1; i++) {
+                lastName += inputChar[i];
+            }
+            for (int i = placeSpace1+1; i < placeSpace2; i++) {
+                firstName += inputChar[i];
+            }
+            for (int i = placeSpace2+1; i < input.length(); i++) {
+                patronymicName += inputChar[i];
+            }
+            System.out.println("Фамилия: " + lastName + "\nИмя: " + firstName + "\nОтчество: " + patronymicName);
+        }
+    }
+}
